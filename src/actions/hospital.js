3b39af2a114166fdc682a63ce08461be6bb87bcf
unbounded3 <jiaenpang54@gmail.com>
2022-08-11 16:15:17 +0800

    “国医堂”组件里的所有内容，都实现了通过请求的方式获取数据，目前是用mock来模拟

--- src/actions/hospital.js
+++ src/actions/hospital.js
@@ -3,14 +3,11 @@ import Taro from "@tarojs/taro";
 export const findHospital = () => {
     return (dispatch) => {
         Taro.request({
-            url: 'https://www.juntaitec.cn/api/gyt/hospitalList', //仅为示例，并非真实的接口地址
-            data: {},
+            url: 'https://www.fastmock.site/mock/3eba8f22893363427fb080b0c20d7b42/weapp/getHospitalList', //仅为示例，并非真实的接口地址
             header: {
                 'content-type': 'application/json' // 默认值
             },
             success: function (res) {
-                // 调reducer修改数据
-                console.log("action执行")
                 dispatch({type: 'searchHospital', hospitalList: res})
             }
         })
@@ -26,5 +23,19 @@ export const changeSelector = (value) => {
 export const setSelectIndex = (value) => {
     return (dispatch) => {
         dispatch({type: 'setSelectIndex', selectIndex: value})
+    }
+}
+
+export const getDoctorList = () => {
+    return (dispatch) => {
+        Taro.request({
+            url: 'https://www.fastmock.site/mock/3eba8f22893363427fb080b0c20d7b42/weapp/getDoctorList', //仅为示例，并非真实的接口地址
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success: function (res) {
+                dispatch({type: 'getDoctorList', doctorList: res})
+            }
+        })
     }
 }
