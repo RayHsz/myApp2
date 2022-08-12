6ef771c8f6c35f86d8f8746f0da91f32e72f1688
mc-hjl <1351957159@qq.com>
2022-08-12 16:55:51 +0800

    预约服务(实现按评分降序排列)

--- src/pages/homePage/reservationService/index.jsx
+++ src/pages/homePage/reservationService/index.jsx
@@ -1,16 +1,17 @@
 import {Component} from "react";
 import {Image, View,ScrollView} from "@tarojs/components";
 import {connect} from "react-redux";
-import {findHospital,setSelectIndex} from '../../../actions/hospital'
+import {findHospital,setSelectIndex,changeSelector} from '../../../actions/hospital'
 import './index.scss'
 import Taro from "@tarojs/taro";
 import {AtSearchBar, AtAccordion, AtList, AtListItem, AtRate} from 'taro-ui'
+import TabBar from "../../tabBarPage";
 
 
 
 
 
-@connect(({hospital}) => ({hospital}), ({findHospital,setSelectIndex}))
+@connect(({hospital}) => ({hospital}), ({findHospital,setSelectIndex,changeSelector}))
 class Index extends Component {
     constructor () {
         super(...arguments)
@@ -21,16 +22,60 @@ class Index extends Component {
         }
     }
 
+    /*搜索框事件*/
     SearchBar=(value)=>{
         this.setState({
             value: value
         })
     }
+
+    /*筛选框事件*/
     handleClick=(value)=> {
         this.setState({
             open: value
         })
     }
+
+    sFilter = (value) => {
+        console.log(value);
+        this.props.changeSelector(this.props.hospital.selector[value]);
+        this.sort(value);
+    }
+
+    sort=(select)=>{
+        /*
+        0:按评分降序
+        1:按评分升序
+         */
+        let a = this.props.hospital.data;
+        let temp;
+        switch (select){
+            case '2':
+                console.log('3123');
+                a.map((num,index) => {
+                    for (let i = 1 ; i < a.length-index ; i++){
+                        if (a[i-1].score<a[i].score){
+                            temp = a[i-1];
+                            a[i-1] = a[i];
+                            a[i] = temp;
+                        }
+                    }
+                });
+                break;
+            case '3':
+                a.map((num,index) => {
+                    for (let i = 1 ; i < a.length-index ; i++){
+                        if (a[i-1].score>a[i].score){
+                            temp = a[i-1];
+                            a[i-1] = a[i];
+                            a[i] = temp;
+                        }
+                    }
+                })
+        }
+    }
+
+    /*医院信息跳转事件*/
     hospitalInformation=(value)=>{
         this.props.setSelectIndex(value);
         Taro.navigateTo({
@@ -62,11 +107,12 @@ class Index extends Component {
                     onClick={this.handleClick.bind(this)}
                     title='筛选'
                     >
-                    <AtList hasBorder={false}>
+                    <AtList hasBorder={false} className='aList'>
                         <AtListItem
                             title='评分'
                             arrow='right'
                             thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
+                            onClick={this.sFilter.bind(this,'2')}
                         />
                         <AtListItem
                             title='剩余量'
@@ -80,6 +126,7 @@ class Index extends Component {
                     {hospitalData}
                 </ScrollView>
                 </View>
+                <TabBar tabBarCurrent={0}/>
             </View>
         )
     }
