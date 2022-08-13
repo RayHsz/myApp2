ccbd5be9c5e44a36110a51e1716e46c9c8505c35
mc-hjl <1351957159@qq.com>
2022-08-13 17:26:56 +0800

    改进代码，清除了bug

--- src/pages/homePage/reservationService/index.jsx
+++ src/pages/homePage/reservationService/index.jsx
@@ -11,6 +11,7 @@ import TabBar from "../../tabBarPage";
 
 
 
+
 @connect(({hospital}) => ({hospital}), ({findHospital,setSelectIndex,changeSelector}))
 class Index extends Component {
     constructor () {
@@ -20,6 +21,10 @@ class Index extends Component {
             open:false,
             hosDisMap:new Map()
         }
+    }
+    /*获取医院列表*/
+    getHospitalList() {
+        this.props.findHospital();
     }
 
     /*搜索框事件*/
@@ -47,7 +52,7 @@ class Index extends Component {
         0:按评分降序
         1:按评分升序
          */
-        let a = this.props.hospital.data;
+        let a = this.props.hospital.hospitalList;
         let temp;
         switch (select){
             case '2':
@@ -84,51 +89,60 @@ class Index extends Component {
     }
 
     render() {
-        const hospitalData=this.props.hospital.data.map((info,index)=>
-            {
-                return (
-                    <View className='hospital' onClick={this.hospitalInformation.bind(this,index)}>
-                        <Image src={info.image} className='hospitalPhoto'/>
-                        <View><text>{info.name}</text></View>
-                        <AtRate value={info.score}/>
-                        <View><text>剩余量:{info.remainder}</text></View>
+        if (this.props.hospital.hospitalList.length === 0) {
+            this.getHospitalList();
+        } else {
+            const hospitalData = this.props.hospital.hospitalList.map((info, index) => {
+                    return (
+                        <View className='hospital' onClick={this.hospitalInformation.bind(this, index)}>
+                            <Image src={info.image} className='hospitalPhoto'/>
+                            <View>
+                                <text>{info.name}</text>
+                            </View>
+                            <AtRate value={info.score}/>
+                            <View>
+                                <text>剩余量:{info.remainder}</text>
+                            </View>
+                        </View>
+                    )
+                }
+            )
+            return (
+                <View className='index'>
+                    <AtSearchBar className='SearchBar' value={this.state.value} onChange={this.SearchBar.bind(this)}/>
+                    <View className='filterBox'>
+                        <View className='type'>
+                            <text>医院:</text>
+                        </View>
+                        <View className='filter'>
+                            <AtAccordion className='atAccordion'
+                                         open={this.state.open}
+                                         onClick={this.handleClick.bind(this)}
+                                         title='筛选'
+                            >
+                                <AtList hasBorder={false} className='aList'>
+                                    <AtListItem
+                                        title='评分'
+                                        arrow='right'
+                                        thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
+                                        onClick={this.sFilter.bind(this, '2')}
+                                    />
+                                    <AtListItem
+                                        title='剩余量'
+                                        arrow='right'
+                                        thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
+                                    />
+                                </AtList>
+                            </AtAccordion>
+                        </View>
+                        <ScrollView className='hospitalList'>
+                            {hospitalData}
+                        </ScrollView>
                     </View>
-                )
-            }
-        )
-        return (
-            <View className='index'>
-                <AtSearchBar className='SearchBar' value={this.state.value} onChange={this.SearchBar.bind(this)}/>
-                <View className='filterBox'>
-                <View className='type'><text>医院:</text></View>
-                <View className='filter'>
-                    <AtAccordion className='atAccordion'
-                    open={this.state.open}
-                    onClick={this.handleClick.bind(this)}
-                    title='筛选'
-                    >
-                    <AtList hasBorder={false} className='aList'>
-                        <AtListItem
-                            title='评分'
-                            arrow='right'
-                            thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
-                            onClick={this.sFilter.bind(this,'2')}
-                        />
-                        <AtListItem
-                            title='剩余量'
-                            arrow='right'
-                            thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
-                        />
-                    </AtList>
-                </AtAccordion>
+                    <TabBar tabBarCurrent={0}/>
                 </View>
-                <ScrollView className='hospitalList'>
-                    {hospitalData}
-                </ScrollView>
-                </View>
-                <TabBar tabBarCurrent={0}/>
-            </View>
-        )
+            )
+        }
     }
 }
 
